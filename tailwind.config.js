7752819a74c2894fb65c78fff226fdd84d75d54a
unknown <mizurisysthensis3@gmail.com>
2024-08-01 07:50:01 +0700

    fix all

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,10 @@ module.exports = {
       },
       colors: {
         htm: "#131313",
+        coklat: "#914F1E",
+        cream: "#DEAC80",
+        creamwhite: "#F7DCB9",
+        sage: "#B5C18E",
       },
     },
   },
